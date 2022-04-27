39f92bdd111576700351cf2822c883eb57b17025
dodni <feherdonat99@gmail.com>
2022-04-28 00:41:08 +0200

    EP32 03:56s

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,45 @@
-@extends('components.layout')
+<x-layout>
+    @include('_posts-header')
 
-@section('banner')
-    <h1>My Blog</h1>
-@endsection
 
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
-@endsection
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card :post="$posts[0]"/>
+
+{{--        <div class="lg:grid lg:grid-cols-2">--}}
+{{--            <x-post-card />--}}
+{{--            <x-post-card />--}}
+
+{{--        </div>--}}
+
+{{--        <div class="lg:grid lg:grid-cols-3">--}}
+{{--            <x-post-card />--}}
+{{--            <x-post-card />--}}
+{{--            <x-post-card />--}}
+{{--        </div>--}}
+    </main>
+
+
+
+    {{--    @section('banner')--}}
+{{--        <h1>My Blog</h1>--}}
+{{--    @endsection--}}
+
+{{--    @section('content')--}}
+{{--        @foreach ($posts as $post)--}}
+{{--            <article>--}}
+{{--                <h1>--}}
+{{--                    <a href="/posts/{{ $post->slug }}">--}}
+{{--                        {{ $post->title }}--}}
+{{--                    </a>--}}
+{{--                </h1>--}}
+{{--                <p>--}}
+{{--                    By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--                </p>--}}
+{{--                <div>--}}
+{{--                    {{ $post->excerpt }}--}}
+{{--                </div>--}}
+{{--            </article>--}}
+{{--        @endforeach--}}
+{{--    @endsection--}}
+</x-layout>
+
