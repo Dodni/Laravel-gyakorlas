36a23822b271171096b9eb735ff0490f0f9553ab
dodni <feherdonat99@gmail.com>
2022-04-29 00:22:45 +0200

    ep33 next day continue

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,43 +3,12 @@
 
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card :post="$posts[0]"/>
+        @if ($posts->count())
+            <x-post-grid :posts="$posts"></x-post-grid>
 
-{{--        <div class="lg:grid lg:grid-cols-2">--}}
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
-
-{{--        </div>--}}
-
-{{--        <div class="lg:grid lg:grid-cols-3">--}}
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
-{{--        </div>--}}
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
     </main>
-
-
-
-    {{--    @section('banner')--}}
-{{--        <h1>My Blog</h1>--}}
-{{--    @endsection--}}
-
-{{--    @section('content')--}}
-{{--        @foreach ($posts as $post)--}}
-{{--            <article>--}}
-{{--                <h1>--}}
-{{--                    <a href="/posts/{{ $post->slug }}">--}}
-{{--                        {{ $post->title }}--}}
-{{--                    </a>--}}
-{{--                </h1>--}}
-{{--                <p>--}}
-{{--                    By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
-{{--                </p>--}}
-{{--                <div>--}}
-{{--                    {{ $post->excerpt }}--}}
-{{--                </div>--}}
-{{--            </article>--}}
-{{--        @endforeach--}}
-{{--    @endsection--}}
 </x-layout>
 
